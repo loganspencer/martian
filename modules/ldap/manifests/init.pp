892d3090a132139be6fc0a56f0e029e7594c8e2a
Logan Spencer <logan.spencer@gmail.com>
2012-03-07 23:42:34 -0600

    deleting shit

--- modules/ldap/manifests/init.pp
+++ modules/ldap/manifests/init.pp
@@ -1,19 +1,17 @@
 # Class: ldap
 #
-# This module manages LDAP Server and Clients..
+# This module manages LDAP Server and Clients.
 #
 # Parameters:
-# *client* - Binary Flag (true|false) to configure an LDAP Client 
+# *client* - Binary Flag (true|false) to configure an LDAP Client
 # *server* - Binary Flag (true|false) to configure an LDAP Server
-# *server_type* - (openldap|389) configures different types of 
-#                 LDAP servers. Default is OpenLDAP Setup
 # *ssl*         - (true|false) - enable SSL Support. *IN DEVELOPMENT*
 #
 # Actions:
 #
 # Requires:
 #  puppetlabs-stdlib - https://github.com/puppetlabs/puppetlabs-stdlib
-#  
+#
 #  Packaged LDAP
 #    - RHEL: EPEL or custom package
 #    - Debian/Ubuntu: Default Install or custom package
@@ -23,23 +21,22 @@
 #
 # Setup (Bootstrap) and Configuration of this module are currently 
 # separated in order to allow for multiple LDAP server definitions
-# and multiple LDAP Trees being managed. 
+# and multiple LDAP Trees being managed.
 #
 # Bootstrap:
 # node 'server.puppetlabs.test' {
 #   class { 'ldap':
 #     server      => 'true',
-#     server_type => 'openldap',
 #     ssl         => 'false',
 #   }
 # }
 # node 'client.puppetlabs.test' {
 #   class {'ldap':
-#     client  => 'true', 
-#     ssl     => 'false',  
+#     client  => 'true',
+#     ssl     => 'false',
 #   }
 # }
-# 
+#
 # Server Configuration:
 # ldap::define::domain {'puppetlabs.test':
 #   basedn   => 'dc=puppetlabs,dc=test',
@@ -60,37 +57,38 @@ class ldap(
   $ssl         = 'false',
   $ssl_ca      = '',
   $ssl_cert    = '',
-  $ssl_key     = '',
-  $server_type = 'openldap'
+  $ssl_key     = ''
 ) {
   include stdlib
   include ldap::params
-  
+
   # Take advantage of the Anchor Pattern.
   anchor { 'ldap::begin': }
-  anchor { 'ldap::end': }
-  
-  Class {
-    require => Anchor['ldap::begin'],
-    before  => Anchor['ldap::end'],
-  }
+  -> anchor { 'ldap::begin::client': }
+  -> anchor { 'ldap::end::client': }
+  -> anchor { 'ldap::begin::server': }
+  -> anchor { 'ldap::end::server': }
+  -> anchor { 'ldap::end': }
 
   # Define Client Specific Information
   if $client == 'true' {
     class { 'ldap::client':
       ensure => 'present',
       ssl    => $ssl,
+      require => Anchor['ldap::begin::client'],
+      before  => Anchor['ldap::end::client'],
     }
   }
-  
+
   # Define Server Specific Information
   if $server == 'true' {
     class { 'ldap::server': 
-      server_type => $server_type,
       ssl         => $ssl,
       ssl_ca      => $ssl_ca,
       ssl_cert    => $ssl_cert,
-      ssl_key     => $ssl_key
+      ssl_key     => $ssl_key,
+      require     => Anchor['ldap::begin::server'],
+      before      => Anchor['ldap::end::server'],
     }
   }
 }
